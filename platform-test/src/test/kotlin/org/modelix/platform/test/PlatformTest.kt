5d1e5ea64fb0ff76f29e5309b94f66e34466df47
Michael Huster <mhuster@itemis.com>
2023-04-13 09:34:31 +0200

    added gradle platform

---
+++ platform-test/src/test/kotlin/org/modelix/platform/test/PlatformTest.kt
@@ -0,0 +1,11 @@
+package org.modelix.platform.test
+
+import org.junit.Test
+import org.modelix.model.server.handlers.LightModelServer
+
+class PlatformTest {
+    @Test
+    fun `can use light model server`() {
+        val modelServer: LightModelServer? = null
+    }
+}
