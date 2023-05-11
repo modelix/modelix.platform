379556cfef02bde765a78d52f891ac4de42cbae7
Norman Köster <nkoester@itemis.com>
2023-05-11 16:42:06 +0200

    Remove itemis nexus to allow publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -62,18 +62,18 @@ publishing {
         }
     }
     repositories {
-        maven {
-            name = "itemis"
-            url = if (version.toString().contains("SNAPSHOT")) {
-                uri("https://artifacts.itemis.cloud/repository/maven-mps-snapshots/")
-            } else {
-                uri("https://artifacts.itemis.cloud/repository/maven-mps-releases/")
-            }
-            credentials {
-                username = project.findProperty("artifacts.itemis.cloud.user").toString()
-                password = project.findProperty("artifacts.itemis.cloud.pw").toString()
-            }
-        }
+        // maven {
+        //     name = "itemis"
+        //     url = if (version.toString().contains("SNAPSHOT")) {
+        //         uri("https://artifacts.itemis.cloud/repository/maven-mps-snapshots/")
+        //     } else {
+        //         uri("https://artifacts.itemis.cloud/repository/maven-mps-releases/")
+        //     }
+        //     credentials {
+        //         username = project.findProperty("artifacts.itemis.cloud.user").toString()
+        //         password = project.findProperty("artifacts.itemis.cloud.pw").toString()
+        //     }
+        // }
         maven {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/modelix/modelix.platform")
